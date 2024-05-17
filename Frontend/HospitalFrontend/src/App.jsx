98d1b1c71dd539747e594f633be1e87c295368ac
sumedhiscoding <sumedhiscoding@gmail.com>
2024-05-18 01:03:29 +0530

    removed external node-modules

--- Frontend/HospitalFrontend/src/App.jsx
+++ Frontend/HospitalFrontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
-
+import {useNavigate} from "react-router-dom"
 function App() {
-
+  const navigate=useNavigate();
   return (
+
     <>
     <h1 className="bg-red-600">THis is my website</h1>
+    <button>This is a navigate button</button>
     </>
   );
 }
