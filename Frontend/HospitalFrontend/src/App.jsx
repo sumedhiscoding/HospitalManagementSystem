e11c56b86c6515038bc5e8a1ce997c16bf96e595
sumedhiscoding <sumedhiscoding@gmail.com>
2024-05-18 00:50:36 +0530

    initial commit

---
+++ Frontend/HospitalFrontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+    <h1 className="bg-red-600">THis is my website</h1>
+    </>
+  );
+}
+
+export default App;
